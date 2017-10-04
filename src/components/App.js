ec8545570c4f54c0d11e14b3b1a0abf8df33c4ea
Mike Tsou <mr.tsou@gmail.com>
2017-10-03 23:32:03 -0700

    Make youtube api calls with axios in the App component

--- src/components/App.js
+++ src/components/App.js
@@ -1,8 +1,25 @@
 import React, { Component } from 'react';
+import axios from 'axios';
+import keys from '../../config/keys';
 
 import SearchBar from './SearchBar';
 
 class App extends Component {
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      videos: [],
+      selectedVideo: null
+    };
+
+    axios.get(`https://www.googleapis.com/youtube/v3/search?key=${keys.youtubeAPIKey}&part=snippet&q=warriors&id&order=date&maxResults=20`)
+      .then((videos) => this.setState({
+        videos: videos.data.items,
+        selectedVideo: videos[0]
+      }));
+  }
+
   render() {
     return (
       <div>
